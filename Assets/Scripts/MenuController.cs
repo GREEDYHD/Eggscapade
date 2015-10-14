46072d71a72d163e2459481ae6aeb0ce8692305a
gREEDYHD <dAVIDGREEDY1995GMAIL.COM>
2015-10-14 17:48:07 +0100

    Menu added, inputs fixed
    Signed-off-by: Youles <Chrisyoules93@gmail.com>

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour 
+{
+	
+	public void LoadLevel(string name)
+	{
+		Application.LoadLevel (name);
+	}
+	
+	public void Quit()
+	{
+		Application.Quit ();
+	}
+	
+}
